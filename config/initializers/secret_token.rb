2ae604fdbe253893d5e76eca1a74df821a856bb5
Kaloyan Yordanov <kaloyan.yordanov@dbrdev.com>
2013-05-29 18:17:53 +0300

    Model testing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RspecApp::Application.config.secret_token = 'f67d74bc93aefdc37c7bf5cfe83893f446e1585ea0e18270808bf87f8787c712e89fa4dbe01603b35da5a5c8733652c4bdad73061b53e2440db02d12da00bc40'
---
+++ spec/models/contact_spec.rb
@@ -0,0 +1,67 @@
+require 'spec_helper'
+
+describe Contact do
+  it "is valid with a firstname, lastname and email" do
+    contact = Contact.new(firstname: 'Aaron', lastname: 'Sumner',
+      email: "asd@asd.asd")
+    expect(contact).to be_valid
+  end
+
+  it "is invalid without a firstname" do
+    expect(Contact.new(firstname: nil)).to have(1).errors_on(:firstname)
+  end
+
+  it "is invalid without a lastname" do
+    expect(Contact.new(lastname: nil)).to have(1).errors_on(:lastname)
+  end
+
+  it "is invalid with a duplicate email address" do
+    Contact.create(firstname: 'Joe', lastname: 'Tester',
+      email: 'tester@example.com')
+    contact = Contact.new(firstname: 'Jane', lastname: 'Tester',
+      email: 'tester@example.com')
+    expect(contact).to have(1).errors_on(:email)
+  end
+
+  it "returns a contact's full name as a string" do
+    contact = Contact.new(firstname: 'John', lastname: 'Doe',
+      email: 'johndoe@example.com')
+    expect(contact.name).to eq 'John Doe'
+  end
+
+  # this is not needed anymore
+  it "returns a sorted array of results that match" do
+    smith = Contact.create(firstname: 'John', lastname: 'Smith',
+      email: 'jsmith@example.com')
+    jones = Contact.create(firstname: 'Tim', lastname: 'Jones',
+      email: 'tjones@example.com')
+    johnson = Contact.create(firstname: 'John', lastname: 'Johnson',
+      email: 'jjohnson@example.com')
+
+    #expect(Contact.by_letter("J")).to eq [jones, johnson]
+    expect(Contact.by_letter("J")).to_not include smith
+  end
+
+  describe "filter last name by letter" do
+    before :each do
+      @smith = Contact.create(firstname: 'John', lastname: 'Smith',
+        email: 'jsmith@example.com')
+      @jones = Contact.create(firstname: 'Tim', lastname: 'Jones',
+        email: 'tjones@example.com')
+      @johnson = Contact.create(firstname: 'John', lastname: 'Johnson',
+        email: 'jjohnson@example.com')
+    end
+
+    context "matching letters" do
+      it "returns a sorted array of results that match" do
+        expect(Contact.by_letter("J")).to match_array [@johnson, @jones]
+      end
+    end
+
+    context "non-matching letters" do
+      it "only returns contacts with the provided starting letter" do
+        expect(Contact.by_letter("J")).to_not include @smith
+      end
+    end
+  end
+end
